43543ebca6ed162e0c7b409a9e00af37ed3aeb1a
Filza Shahid <filzashahid2001@gmail.com>
2023-02-22 21:55:04 +0500

    corrected module uploaded

--- h264/src/inter_prediction/sad_absolute.sv
+++ h264/src/inter_prediction/sad_absolute.sv
@@ -17,8 +17,8 @@ module sad_absolute #
 logic [PIX_WIDTH-1:0] out0, out1, out2, out3;
 
 logic [PIX_WIDTH-1:0] S4x4 [0:3][0:3];
-logic [PIX_WIDTH-1:0] S4x8 [0:3][0:1];
-logic [PIX_WIDTH-1:0] S8x4 [0:1][0:3];
+logic [PIX_WIDTH-1:0] S4x8 [0:1][0:3];
+logic [PIX_WIDTH-1:0] S8x4 [0:3][0:1];
 logic [PIX_WIDTH-1:0] S8x8 [0:1][0:1];
 logic [PIX_WIDTH-1:0] S16x8 [0:1];
 logic [PIX_WIDTH-1:0] S8x16 [0:1];
@@ -38,35 +38,35 @@ generate
             adder_tree add0
             (
                 .in1(sad[i][j]),
-                .in2(sad[i+1][j]),
-                .in3(sad[i+2][j]),
-                .in4(sad[i+3][j]),
+                .in2(sad[i][j+1]),
+                .in3(sad[i][j+2]),
+                .in4(sad[i][j+3]),
                 .temp(out0)
             );
 
             adder_tree add1
             (
-                .in1(sad[i][j+1]),
+                .in1(sad[i+1][j]),
                 .in2(sad[i+1][j+1]),
-                .in3(sad[i+2][j+1]),
-                .in4(sad[i+3][j+1]),
+                .in3(sad[i+1][j+2]),
+                .in4(sad[i+1][j+3]),
                 .temp(out1)
             ); 
 
             adder_tree add2
             (
-                .in1(sad[i][j+2]),
-                .in2(sad[i+1][j+2]),
+                .in1(sad[i+2][j]),
+                .in2(sad[i+2][j+1]),
                 .in3(sad[i+2][j+2]),
-                .in4(sad[i+3][j+2]),
+                .in4(sad[i+2][j+3]),
                 .temp(out2)
             );
 
             adder_tree add3
             (
-                .in1(sad[i][j+3]),
-                .in2(sad[i+1][j+3]),
-                .in3(sad[i+2][j+3]),
+                .in1(sad[i+3][j]),
+                .in2(sad[i+3][j+1]),
+                .in3(sad[i+3][j+2]),
                 .in4(sad[i+3][j+3]),
                 .temp(out3)
             );
@@ -101,38 +101,12 @@ shift_register_S4x4 #
         .WIDTH(PIX_WIDTH),
         .NUM_OF_REG(4)
     )
-    shift4x4_10
+    shift4x4_01
     (
         .rst(rst), 
         .clk(clk),
-        .data_in(S4x4[1][0]),
+        .data_in(S4x4[0][1]),
         .data_out(shifted_S4x4[1])
-    );
-
-shift_register_S4x4 #
-    (
-        .WIDTH(PIX_WIDTH),
-        .NUM_OF_REG(4)
-    )
-    shift4x4_20
-    (
-        .rst(rst), 
-        .clk(clk),
-        .data_in(S4x4[2][0]),
-        .data_out(shifted_S4x4[2])
-    );   
-
-shift_register_S4x4 #
-    (
-        .WIDTH(PIX_WIDTH),
-        .NUM_OF_REG(4)
-    )
-    shift4x4_30
-    (
-        .rst(rst), 
-        .clk(clk),
-        .data_in(S4x4[3][0]),
-        .data_out(shifted_S4x4[3])
     );
 
 shift_register_S4x4 #
@@ -145,6 +119,32 @@ shift_register_S4x4 #
         .rst(rst), 
         .clk(clk),
         .data_in(S4x4[0][2]),
+        .data_out(shifted_S4x4[2])
+    );   
+
+shift_register_S4x4 #
+    (
+        .WIDTH(PIX_WIDTH),
+        .NUM_OF_REG(4)
+    )
+    shift4x4_03
+    (
+        .rst(rst), 
+        .clk(clk),
+        .data_in(S4x4[0][3]),
+        .data_out(shifted_S4x4[3])
+    );
+
+shift_register_S4x4 #
+    (
+        .WIDTH(PIX_WIDTH),
+        .NUM_OF_REG(4)
+    )
+    shift4x4_20
+    (
+        .rst(rst), 
+        .clk(clk),
+        .data_in(S4x4[2][0]),
         .data_out(shifted_S4x4[4])
     );
     
@@ -153,11 +153,11 @@ shift_register_S4x4 #
         .WIDTH(PIX_WIDTH),
         .NUM_OF_REG(4)
     )
-    shift4x4_12
+    shift4x4_21
     (
         .rst(rst), 
         .clk(clk),
-        .data_in(S4x4[1][2]),
+        .data_in(S4x4[2][1]),
         .data_out(shifted_S4x4[5])
     );
 
@@ -179,25 +179,25 @@ shift_register_S4x4 #
         .WIDTH(PIX_WIDTH),
         .NUM_OF_REG(4)
     )
-    shift4x4_32
+    shift4x4_23
     (
         .rst(rst), 
         .clk(clk),
-        .data_in(S4x4[3][2]),
+        .data_in(S4x4[2][3]),
         .data_out(shifted_S4x4[7])
     );
 
 generate
-    for (k = 0; k < PEY/4; k = k + 2)
+    for (k = 0; k < PEX/4; k = k + 2)
     begin
-        for (l = 0; l < PEX/4; l = l + 1)
+        for (l = 0; l < PEY/4; l = l + 1)
         begin
             if (k == 2)
             begin
                 adder sum4x8
                 (
                     .in1(shifted_S4x4[l+4]),
-                    .in2(S4x4[l][k+1]),
+                    .in2(S4x4[k+1][l]),
                     .temp(S4x8[k/2][l])
                 );
             end
@@ -206,7 +206,7 @@ generate
                 adder sum4x8
                 (
                     .in1(shifted_S4x4[l]),
-                    .in2(S4x4[l][k+1]),
+                    .in2(S4x4[k+1][l]),
                     .temp(S4x8[k/2][l])
                 );
             end
@@ -233,8 +233,8 @@ generate
             begin
                 adder sum8x4
                 (
-                    .in1(S4x4[k][l]),
-                    .in2(S4x4[k+1][l]),
+                    .in1(S4x4[l][k]),
+                    .in2(S4x4[l][k+1]),
                     .temp(S8x4[l][k/2])
                 );
             end            
@@ -251,7 +251,7 @@ generate
             (
                 .in1(S8x4[n][m]),
                 .in2(S8x4[n+1][m]),
-                .temp(S8x8[m][n/2])
+                .temp(S8x8[n/2][m])
             );
         end
     end
@@ -330,20 +330,20 @@ generate
 endgenerate
 
 assign S4x4_00 = S4x4[0][0];
-assign S4x4_01 = S4x4[1][0];
-assign S4x4_02 = S4x4[2][0];
-assign S4x4_03 = S4x4[3][0];
-assign S4x4_10 = S4x4[0][1];
+assign S4x4_01 = S4x4[0][1];
+assign S4x4_02 = S4x4[0][2];
+assign S4x4_03 = S4x4[0][3];
+assign S4x4_10 = S4x4[1][0];
 assign S4x4_11 = S4x4[1][1];
-assign S4x4_12 = S4x4[2][1];
-assign S4x4_13 = S4x4[3][1];
-assign S4x4_20 = S4x4[0][2];
-assign S4x4_21 = S4x4[1][2];
+assign S4x4_12 = S4x4[1][2];
+assign S4x4_13 = S4x4[1][3];
+assign S4x4_20 = S4x4[2][0];
+assign S4x4_21 = S4x4[2][1];
 assign S4x4_22 = S4x4[2][2];
-assign S4x4_23 = S4x4[3][2];
-assign S4x4_30 = S4x4[0][3];
-assign S4x4_31 = S4x4[1][3];
-assign S4x4_32 = S4x4[2][3];
+assign S4x4_23 = S4x4[2][3];
+assign S4x4_30 = S4x4[3][0];
+assign S4x4_31 = S4x4[3][1];
+assign S4x4_32 = S4x4[3][2];
 assign S4x4_33 = S4x4[3][3];
 
 assign S4x8_00 = S4x8[0][0];
