00e06b76b7ad6986da5d1a6773ee33d7ab29511a
FariaaFaheem <fariaa.faheem@gmail.com>
2023-05-01 00:16:18 +0500

    update: mode decision code added

--- h264/main_code/h264topsim.sv
+++ h264/main_code/h264topsim.sv
@@ -152,6 +152,12 @@ module h264topsim();
     logic nop3; //No operation
     logic nop4; //No operation
 
+    // Mode Decision
+    integer frame_distance = 3;
+    logic pred_type; // 0->I, 1->P
+    pred_type = ((framenum % (frame_distance + 1)) == 0) ? 0 : 1;
+
+
     h264intra4x4 intra4x4
     (
         .CLK(clk2), 
